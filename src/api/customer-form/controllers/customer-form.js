2d75c327a678b1d73086057845ca8ad526af475c
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-02-05 15:28:25 +0100

    new form

---
+++ src/api/customer-form/controllers/customer-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * customer-form controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::customer-form.customer-form');
---
+++ src/api/customer-form/services/customer-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * customer-form service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::customer-form.customer-form');
