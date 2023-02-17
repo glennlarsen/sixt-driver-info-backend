33e437d6ea000547a6884753865ed27552f407e4
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-02-17 15:04:22 +0100

    cors settings

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,20 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = {
+  load: [
+    'strapi::errors',
+    'strapi::security',
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ],
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['*']
+    },
+  },
+}
